b0c3cc717a871c73fb79dcb2d7a2f18bec02ba28
khamidullo <khkhamidullo@gmail.com>
2025-10-24 20:22:40 +0500

    feat: inital commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { Bot } = require("grammy")
 
-const bot = new Bot("8235625832:AAF1zqqSrwRy_4QhYW5X-jsF7DHJEFYzn9Y")
+const bot = new Bot("8235625832:AAEujoq41da56ujlCL-96P3-pTQJKu58cTM")
 
 // Reply to any message with "Hi there!".
 bot.on("message", (ctx) => ctx.reply("Hi there!"))
